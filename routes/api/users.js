161ac7447f96cb5b9214c70660a4c32815864fbf
Rahul <rr01012000@gmail.com>
2021-05-13 14:49:30 +0530

    Added User authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router= express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
@@ -21,20 +23,32 @@ router.post('/',[
     const {name, email, password} = req.body;
     try{
         let user = await User.findOne({ email });
+
         //If User already exists
         if(user){
             return res.status(400).json({errors: [{msg: 'User already exists'}]});
         }
+
         //Get users Gravatar
         const avatar = gravatar.url(email,{s: '200',r:'pg',d:'mm'});
         user = new User({ name, email, avatar, password});
+
         //Encrypt Password
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
-        //Return jsonwebtoken
 
-        res.send('User Registered')
+        //Return jsonwebtoken
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'),{ expiresIn: 360000}, (err,token) => {
+            if(err) throw err;
+            res.json({token});
+        });
+        
     }catch(err){
         console.log(err.message);
         res.status(500).send('Server Error');
